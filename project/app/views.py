012129bfbba195a443e3a95a2db174b6d6964a58
eldin61 <eldin.331@gmail.com>
2015-11-26 23:15:59 +0100

    nog wat kleine aanpassingen

--- project/app/views.py
+++ project/app/views.py
@@ -1,12 +1,12 @@
+from flask import render_template, flash, redirect
 from app import app
-from flask import render_template
+from .forms import LoginForm
 import urllib, json
 
-@app.route("/")
-@app.route("/index")
+@app.route("/index", methods=['GET', 'POST'])
 
 def index():
-    url = "http://localhost/index.php/connections//?where[0][col]=value&where[0][op]==&where[0][val]=false&limit=50"
+    url = "http://localhost/index.php/events//?where[0][col]=value&where[0][op]==&where[0][val]=true&limit=50"
     response = urllib.urlopen(url)
     data = json.loads(response.read())
     i = 0
@@ -18,7 +18,7 @@ def index():
         dataArray = data["data"][i]
         unitid = dataArray["unitid"]
 
-        url2 = "http://localhost/index.php/connections//?where[0][col]=value&where[0][op]==&where[0][val]=false&where[1][col]=unitid&where[1][op]==&where[1][val]=" + unitid
+        url2 = "http://localhost/index.php/events//?where[0][col]=value&where[0][op]==&where[0][val]=true&where[1][col]=unitid&where[1][op]==&where[1][val]=" + str(unitid)
         response2 = urllib.urlopen(url2)
         data2 = json.loads(response2.read())
 
@@ -28,6 +28,7 @@ def index():
         vehicles.append(mydict.copy())
 
         i = i + 1
+    form = LoginForm()
 
 
-    return render_template("index.html", vehicles = vehicles)
+    return render_template("index.html", vehicles = vehicles, form = form)
