53b5778d12115834d255e2533b196214463fa290
eldin61 <eldin.331@gmail.com>
2015-11-29 01:11:23 +0100

    login test en unique resultaten

--- project/app/test.py
+++ project/app/test.py
@@ -1,18 +1,19 @@
-import urllib, json
-
-url = "http://localhost/index.php/connections//?where[0][col]=value&where[0][op]==&where[0][val]=false"
+url = "http://localhost/index.php/events//?where[0][col]=value&where[0][op]==&where[0][val]=true&limit=50"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
 i = 0
 
 vehicles = []
 mydict = {}
+orderunit = []
 
 while i < len(data["data"]):
     dataArray = data["data"][i]
     unitid = dataArray["unitid"]
+    orderunit.append(unitid)
 
-    url2 = "http://localhost/index.php/connections//?where[0][col]=value&where[0][op]==&where[0][val]=false&where[1][col]=unitid&where[1][op]==&where[1][val]=" + unitid
+
+    url2 = "http://localhost/index.php/events//?where[0][col]=value&where[0][op]==&where[0][val]=true&where[1][col]=unitid&where[1][op]==&where[1][val]=" + str(unitid)
     response2 = urllib.urlopen(url2)
     data2 = json.loads(response2.read())
 
@@ -20,8 +21,5 @@ while i < len(data["data"]):
 
     mydict = {"id": str(unitid), "ignitions": str(total)}
     vehicles.append(mydict.copy())
-    print vehicles
 
     i = i + 1
-
-print vehicles
--- project/app/views.py
+++ project/app/views.py
@@ -2,33 +2,51 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 import urllib, json
+from flask import request
+from flask.ext.login import login_user, logout_user, current_user, login_required
 
-@app.route("/index", methods=['GET', 'POST'])
-
+@app.route("/login", methods=["POST", "GET"])
 def index():
-    url = "http://localhost/index.php/events//?where[0][col]=value&where[0][op]==&where[0][val]=true&limit=50"
+    return render_template("login.html")
+
+@app.route("/profile", methods=["POST", "GET"])
+def login():
+    username = request.form["sendUser"]
+    password = request.form["sendPass"]
+    if username == "Eldin":
+        if password == "root":
+            return render_template("index.html")
+
+@app.route("/test", methods=["POST", "GET"])
+def test():
+
+    url = "http://localhost/index.php/events//?where[0][col]=value&where[0][op]==&where[0][val]=true"
     response = urllib.urlopen(url)
-    data = json.loads(response.read())
+    jsonObj = json.loads(response.read())
+
     i = 0
-
-    vehicles = []
-    mydict = {}
-
-    while i < len(data["data"]):
-        dataArray = data["data"][i]
-        unitid = dataArray["unitid"]
-
-        url2 = "http://localhost/index.php/events//?where[0][col]=value&where[0][op]==&where[0][val]=true&where[1][col]=unitid&where[1][op]==&where[1][val]=" + str(unitid)
+    unitid = []
+    while i < len(jsonObj["data"]):
+        unitid.append(jsonObj["data"][i]["unitid"])
+        i = i + 1
+    unitid2 = list(set(unitid))
+    j = 0
+    data = []
+    totalDict = {}
+    while j < len(unitid2):
+        url2 = "http://localhost/index.php/events//?where[0][col]=value&where[0][op]==&where[0][val]=true&where[1][col]=unitid&where[1][op]==&where[1][val]=" + str(unitid2[j])
         response2 = urllib.urlopen(url2)
         data2 = json.loads(response2.read())
 
         total = data2["total"]
+        totalDict = {"id": str(unitid2[j]), "ignitions": str(total)}
+        data.append(totalDict.copy())
+        j = j + 1
 
-        mydict = {"id": str(unitid), "ignitions": str(total)}
-        vehicles.append(mydict.copy())
-
-        i = i + 1
     form = LoginForm()
+    return render_template("index.html", form = form, data = data)
 
-
-    return render_template("index.html", vehicles = vehicles, form = form)
+@app.route("/test2", methods=["POST"])
+def test2():
+    print "clicked test2"
+    print (request.form["testreq"])
