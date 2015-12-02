4e513a61ac4f8188b879a7ca6bade1d120162dbd
eldin61 <eldin.331@gmail.com>
2015-12-02 12:25:08 +0100

    rip python

--- project/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-import os
-from flask.ext.login import LoginManager
-
-app = Flask(__name__)
-app.config.from_object('config')
-
-from app import views
--- project/config.py
+++
@@ -1,4 +0,0 @@
-
-
-WTF_CSRF_ENABLED = False
-SECRET_KEY = 'you-will-never-guess'
