e0f1b5f7b126faac1e762c0c4756287e573884c0
eldin61 <eldin.331@gmail.com>
2015-12-13 13:07:07 +0100

    logout broken, fixed

--- flask/flask/Scripts/activate.bat
+++ flask/flask/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\Eldin\Documents\sb-admin-2-python-master\flask"
+set "VIRTUAL_ENV=C:\Users\Eldin\Documents\School\project5\flask\flask"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
