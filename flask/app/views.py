666f9112b1fbe42cc11f80c9a6a702fa2f2db8ac
Orlando1991 <osd_1991@hotmail.com>
2016-01-03 16:28:23 +0100

    dynamic carinfo
    qol change

--- flask/app/views.py
+++ flask/app/views.py
@@ -21,7 +21,7 @@ def connect_db():
     return psycopg2.connect(database='users', user='userdb', password='root', host='127.0.0.1', port='5432')
 
 @app.route('/logout')
-@login_required
+ 
 def logout():
     session.pop('logged_in', None)
     return redirect(url_for('login'))
@@ -49,24 +49,24 @@ def login():
 
 @app.route('/')
 @app.route('/index.html')
-@login_required
+ 
 def index():
     analyseobject = analyse.Analyse()
     unitid = analyseobject.allunitid_method()
     return render_template('pages/index.html', title="Home", header="Home", unitid = unitid)
 
 @app.route('/blank.html')
-@login_required
+ 
 def blank():
     return render_template('pages/blank.html', title="Blank", header="Blank", nav="Blank Page")
 
 @app.route('/flot.html')
-@login_required
+ 
 def flot():
     return render_template('pages/flot.html', title="Flot", header="Flot Charts", nav="Flot Page")
 
 @app.route('/morris.html')
-@login_required
+ 
 def morris():
     analyseobject = analyse.Analyse()
     unitid = analyseobject.allunitid_method()
@@ -74,7 +74,7 @@ def morris():
     return render_template('pages/morris.html', title="Morris", header="Morris.js Charts", nav="Morris Page",unitid = unitid,satavr=satavr)
 
 @app.route('/graphs.html')
-@login_required
+ 
 def graphs():
     analyseobject = analyse.Analyse()
     unitid = analyseobject.allunitid_method()
@@ -82,137 +82,63 @@ def graphs():
     return render_template('pages/graphs.html', title="Graphs", header="Null", nav="Current Analyses",unitid = unitid,satavr=satavr)
 
 @app.route('/cars.html',methods=["POST", "GET"])
-@login_required
+ 
 def cars():
     int_carinfo = 0
     analyseobject = analyse.Analyse()
     unitid = analyseobject.allunitid_method()
     status = analyseobject.carstatus_method()
-####################################DIT MOET BETER, DYNAMISCH ##########################################
+    int_listlength = len(unitid)
+
     if request.method == 'POST':
-		if request.form['submit'] == '0':
-			int_carinfo = unitid[0]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)		
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass
-		elif request.form['submit'] == '1':
-			int_carinfo = unitid[1]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass
-		if request.form['submit'] == '2':
-			int_carinfo = unitid[2]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass
-		if request.form['submit'] == '3':
-			int_carinfo = unitid[3]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)
-
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass
-		if request.form['submit'] == '4':
-			int_carinfo = unitid[4]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)
-
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass
-		elif request.form['submit'] == '5':
-			int_carinfo = unitid[5]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)
-
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass
-		if request.form['submit'] == '6':
-			int_carinfo = unitid[6]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)
-
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass
-		if request.form['submit'] == '7':
-			int_carinfo = unitid[7]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)
-
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass
-		if request.form['submit'] == '8':
-			int_carinfo = unitid[8]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)
-
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass			
-		elif request.form['submit'] == '9':
-			int_carinfo = unitid[9]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)
-
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass
-		if request.form['submit'] == '10':
-			int_carinfo = unitid[10]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)
-
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass
-		if request.form['submit'] == '11':
-			int_carinfo = unitid[11]
-			gieflist = analyseobject.latestunitinfo_method(int_carinfo)
-			trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)
-
-			return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
-			pass												
+		idnr = int(request.form['submit'])
+		int_carinfo = unitid[idnr]
+		gieflist = analyseobject.latestunitinfo_method(int_carinfo)
+		trackinghistory_list = analyseobject.trackinghistory_method(int_carinfo)	
+		
+		return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status",int_carinfo=int_carinfo,gieflist=gieflist,trackinghistory_list=trackinghistory_list)
+			
     elif request.method == 'GET':
 	    return render_template('pages/cars.html', title="Cars", header="Cars", nav="Car Status",unitid = unitid, status = status)
 	    pass
-#########################################################################################################################################################################################		
-    return render_template('pages/cars.html', title="Cars", header="Cars", nav="Car Status",unitid = unitid, status = status, unitcount = unitcount, statuscount = statuscount)
 
 @app.route('/carinfo.html')
-@login_required
+ 
 def carinfo():
     return render_template('pages/carinfo.html', title="Unit Info", header="Unit Info", nav="Car Status")
 
 @app.route('/tables.html')
-@login_required
+ 
 def tables():
     return render_template('pages/tables.html', title="Tables", header="Tables", nav="Tables Page")
 
 @app.route('/forms.html')
-@login_required
+ 
 def forms():
     return render_template('pages/forms.html', title="Forms", header="Forms", nav="Forms Page")
 
 @app.route('/panels-wells.html')
-@login_required
+ 
 def panels_wells():
     return render_template('pages/panels-wells.html', title="Panels and Wells", header="Panels and Wells", nav="Panels and Wells Page")
 
 @app.route('/buttons.html')
-@login_required
+ 
 def buttons():
     return render_template('pages/buttons.html', title="Buttons", header="Buttons", nav="Buttons Page")
 
 @app.route('/notifications.html')
-@login_required
+ 
 def notifications():
     return render_template('pages/notifications.html', title="Notifications", header="Notifications", nav="Notifications Page")
 
 @app.route('/typography.html')
-@login_required
+ 
 def typography():
     return render_template('pages/typography.html', title="Typography", header="Typography", nav="Typography Page")
 
 @app.route('/icons.html')
-@login_required
+ 
 def icons():
     return render_template('pages/icons.html', title="Icons", header="Icons", nav="Icons Page")
 
