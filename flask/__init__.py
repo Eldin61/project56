4c26ba4ee94a493ddd17fcf7bdbb3c5f9b6036f1
eldin61 <eldin.331@gmail.com>
2016-01-13 10:44:41 +0100

    hoi het heeft gewerkt

--- flask/__init__.py
+++ flask/__init__.py
@@ -2,10 +2,4 @@
 from app import app
 app.run(debug=True)
 
-#test voor commit weer plz automatisch
-#test voor commit
-#plz nu wel auto github
-#salty cunt
-# plz n more report plz
-# plzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
-#plz work now plz plz plz plz plz plz plz plz
+#it worked!
